c3e992876423561d9f6c260e9072fe98f6ba5bdd
Ronny Legones <legonesronny@gmail.com>
2019-04-24 16:35:52 -0400

    Updates

--- precache-manifest.04682f3c386d133ab42eab2bcf520b40.js
+++ precache-manifest.04682f3c386d133ab42eab2bcf520b40.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/directorio-de-negocios/static/js/runtime~main.9b35d40b.js"
   },
   {
-    "revision": "172ff994de30db88884a",
-    "url": "/directorio-de-negocios/static/js/main.666d0d87.chunk.js"
+    "revision": "17f13d615b13c87b3363",
+    "url": "/directorio-de-negocios/static/js/main.8073a635.chunk.js"
   },
   {
     "revision": "97d7eb0a8b74af59fa56",
     "url": "/directorio-de-negocios/static/js/2.beb0e9d9.chunk.js"
   },
   {
-    "revision": "172ff994de30db88884a",
+    "revision": "17f13d615b13c87b3363",
     "url": "/directorio-de-negocios/static/css/main.be1b60e9.chunk.css"
   },
   {
-    "revision": "06a2afe581f682fd8782635275473920",
+    "revision": "670f1ac7fea213b4913e01a55b860e82",
     "url": "/directorio-de-negocios/index.html"
   }
 ];
