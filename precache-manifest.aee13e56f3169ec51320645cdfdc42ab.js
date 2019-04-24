5f543a638b2349f74dde26ca50e01d6a08332e8c
Ronny Legones <legonesronny@gmail.com>
2019-04-24 13:57:17 -0400

    Updates

---
+++ precache-manifest.aee13e56f3169ec51320645cdfdc42ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "252e7fbf49687a45f419",
+    "url": "/directorio-de-negocios/static/js/runtime~main.9b35d40b.js"
+  },
+  {
+    "revision": "9f7a1dc3a069ed987059",
+    "url": "/directorio-de-negocios/static/js/main.c722dc79.chunk.js"
+  },
+  {
+    "revision": "97d7eb0a8b74af59fa56",
+    "url": "/directorio-de-negocios/static/js/2.beb0e9d9.chunk.js"
+  },
+  {
+    "revision": "9f7a1dc3a069ed987059",
+    "url": "/directorio-de-negocios/static/css/main.be1b60e9.chunk.css"
+  },
+  {
+    "revision": "ba01ecf4de54d55481ad911e23a67e78",
+    "url": "/directorio-de-negocios/index.html"
+  }
+];
