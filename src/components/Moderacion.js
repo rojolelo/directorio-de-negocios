ff75ef74c2b12888415335b845bc57331fdd90a2
Ronny Legones <legonesronny@gmail.com>
2019-05-05 19:38:16 -0400

    Minor changes

--- src/components/Moderacion.js
+++ src/components/Moderacion.js
@@ -1,88 +1,93 @@
-import React, { Component } from 'react';
-import ObjetoListaMod from './Listado/ObjetoListaMod';
+import React, { Component } from "react";
+import ObjetoListaMod from "./Listado/ObjetoListaMod";
 import firebase from "firebase";
 import Swal from "sweetalert2";
-
 
 class Moderacion extends Component {
     state = {
         clave: "",
         lista: [],
         email: ""
-    }
+    };
 
     emailRef = React.createRef();
     passwordRef = React.createRef();
 
     componentWillMount() {
-        firebase.database().ref("moderacion").on("value", x => {
-            this.setState({
-                lista: x.val()
-            })
-        })
+        firebase
+            .database()
+            .ref("moderacion")
+            .on("value", x => {
+                this.setState({
+                    lista: x.val()
+                });
+            });
     }
 
-    iniciarSesion = (e) => {
-        e.preventDefault()
+    iniciarSesion = e => {
+        e.preventDefault();
         const email = this.emailRef.current.value;
         const password = this.passwordRef.current.value;
 
-
-        firebase.auth().signInWithEmailAndPassword(email, password)
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
             .catch(error => {
                 var errorCode = error.code;
                 var errorMessage = error.message;
                 if (errorCode === "auth/wrong-password") {
                     Swal.fire({
-                        type: 'error',
-                        title: 'Oops...',
-                        text: 'Wrong Password',
+                        type: "error",
+                        title: "Oops...",
+                        text: "Wrong Password"
                     });
                 } else if (errorCode === "auth/user-not-found") {
                     Swal.fire({
-                        type: 'error',
-                        title: 'Oops...',
-                        text: "Are you sure you're and admin? :)",
+                        type: "error",
+                        title: "Oops...",
+                        text: "Are you sure you're and admin? :)"
                     });
                 } else {
                     Swal.fire({
-                        type: 'error',
-                        title: 'Oops...',
-                        text: errorMessage,
+                        type: "error",
+                        title: "Oops...",
+                        text: errorMessage
                     });
-                    console.log(error)
+                    console.log(error);
                 }
-
             })
             .then(result => {
                 if (result) {
                     this.setState({
                         email: result.user.email
-                    })
+                    });
                 }
-            })
-    }
+            });
+    };
 
     logout = () => {
-        const mainAddress = window.location.href.replace("/listadomoderacion", "");
-        firebase.auth().signOut()
-        document.location.assign(mainAddress)
-    }
+        const mainAddress = window.location.href.replace(
+            "/listadomoderacion",
+            ""
+        );
+        firebase.auth().signOut();
+        document.location.assign(mainAddress);
+    };
 
     listaPorModerar = () => {
         let preLista = [];
         let keysLista = [];
         let lista = [];
 
-        if ((Boolean(this.state.lista))) {
+        if (Boolean(this.state.lista)) {
             preLista = this.state.lista;
 
-            if (typeof (preLista) === "object") {
+            if (typeof preLista === "object") {
                 keysLista = Object.keys(preLista);
             }
 
             for (let i = 0; i < keysLista.length; i++) {
-                lista.push(preLista[keysLista[i]])
+                lista.push(preLista[keysLista[i]]);
             }
         }
 
@@ -90,81 +95,98 @@ class Moderacion extends Component {
             return (
                 <React.Fragment>
                     <tr>
-                        <td>
-                            No hay nada para moderar
-                        </td>
+                        <td>No hay nada para moderar</td>
                     </tr>
                 </React.Fragment>
-            )
-        };
-
+            );
+        }
 
         return (
             <React.Fragment>
-                {
-                    lista.map((objeto, i) => {
-                        return (
-                            <ObjetoListaMod objeto={objeto} key={i} borrar={this.props.borrar} aceptar={this.props.aceptar} />
-                        )
-                    })
-                }
+                {lista.map((objeto, i) => {
+                    return (
+                        <ObjetoListaMod
+                            objeto={objeto}
+                            key={i}
+                            borrar={this.props.borrar}
+                            aceptar={this.props.aceptar}
+                        />
+                    );
+                })}
             </React.Fragment>
-        )
-
-    }
+        );
+    };
 
     render() {
-
-
-
         const logged = Boolean(firebase.auth().currentUser);
-        console.log("Logged", logged)
+        console.log("Logged", logged);
         if (logged === false) {
             return (
                 <React.Fragment>
-                    <div><h2>Insert your Email and Password</h2></div>
-                    <div><h4>You need log in in order to moderate</h4></div>
+                    <div>
+                        <h2>Insert your Email and Password</h2>
+                    </div>
+                    <div>
+                        <h4>You need log in in order to moderate</h4>
+                    </div>
                     <form onSubmit={this.iniciarSesion}>
-                        <div><label>Email</label><input className="form-control" type="text" ref={this.emailRef} placeholder="Email" /></div>
-                        <div><label>Password</label><input className="form-control" type="text" ref={this.passwordRef} placeholder="Password" /></div>
-                        <div><button className="btn btn-primary" type="submit" >Iniciar Sesion</button></div>
+                        <div>
+                            <label>Email</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                ref={this.emailRef}
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                ref={this.passwordRef}
+                                placeholder="Password"
+                            />
+                        </div>
+                        <div>
+                            <button className="btn btn-primary" type="submit">
+                                Iniciar Sesion
+                            </button>
+                        </div>
                     </form>
                 </React.Fragment>
-            )
+            );
         }
 
-        if ((this.state.clave !== "1234")) {
-            return (
-                <React.Fragment>
-                    <div><h2>Insert MPass </h2></div>
-                    <label>Mpassword</label>
-                    <div>
-                        <input type="text" placeholder="password" onChange={e => {
-                            this.setState({
-                                clave: e.target.value
-                            })
-                        }} />
-                    </div>
-                    <div>
-                        <button type="button" className="btn btn-warning" onClick={this.logout}>Logout</button>
-                    </div>
-                </React.Fragment>)
-        }
-
-
+        // if ((this.state.clave !== "1234")) {
+        //     return (
+        //         <React.Fragment>
+        //             <div><h2>Insert MPass </h2></div>
+        //             <label>Mpassword</label>
+        //             <div>
+        //                 <input type="text" placeholder="password" onChange={e => {
+        //                     this.setState({
+        //                         clave: e.target.value
+        //                     })
+        //                 }} />
+        //             </div>
+        //             <div>
+        //                 <button type="button" className="btn btn-warning" onClick={this.logout}>Logout</button>
+        //             </div>
+        //         </React.Fragment>)
+        // }
 
         return (
             <React.Fragment>
                 <h2 className="titulo-moderacion">Moderacion</h2>
 
-
                 <table className="table table-bordered table-striped">
-                    <tbody>
-                        {this.listaPorModerar()}
-                    </tbody>
+                    <tbody>{this.listaPorModerar()}</tbody>
                 </table>
-                <button type="button" onClick={this.logout}>Logout</button>
-            </React.Fragment >
+                <button type="button" onClick={this.logout}>
+                    Logout
+                </button>
+            </React.Fragment>
         );
     }
 }
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,65 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Listado from "./Listado";
 import Add from "./Add";
-import BotonAdd from './BotonAdd';
+import BotonAdd from "./BotonAdd";
 import Moderacion from "./Moderacion";
 import Navegacion from "./Navegacion";
 import firebase from "firebase";
 
 //FIREBASE INIT----------------
 var config = {
-    //PROTECTED INFORMATION
-    // FOR YOU PROJECT USE HERE YOUR OWN FIREBASE DATABASE
+    // PROTECTED INFORMATION
 };
 
 firebase.initializeApp(config);
 
-const db = firebase.database().ref("directorio")
-const dbMod = firebase.database().ref("moderacion")
-
-
+const db = firebase.database().ref("directorio");
+const dbMod = firebase.database().ref("moderacion");
 
 //-------------------------------
-
-
 
 class Router extends Component {
     state = {
         lista: [],
         listaPorModerar: [],
-        loading: true,
-    }
-
+        loading: true
+    };
 
     fromDBtoState = () => {
         //EVERYTIME THERE A UPDATE IT SETS LOADING TO TRUE
-        // ITS ON ANOTHER FUNCTION IN ORDER TO AVOID 
+        // ITS ON ANOTHER FUNCTION IN ORDER TO AVOID
         // this.setState NOT WORKING PROPERLY
-        this.loading()
+        this.loading();
         db.on("value", x => {
             const directorio = x.val();
             const ids = Object.keys(directorio);
             let newState = [];
             for (let i = 0; i < ids.length; i++) {
-                newState.push(directorio[ids[i]])
+                newState.push(directorio[ids[i]]);
             }
             this.setState({
                 lista: newState,
                 loading: false
-            })
-        })
-    }
+            });
+        });
+    };
 
     loading = () => {
         this.setState({
             loading: true
-        })
-    }
+        });
+    };
 
-    separacionPorPaginas = (datos) => {
+    separacionPorPaginas = datos => {
         let prevState = [...datos];
 
         let separacion = [];
@@ -68,143 +62,146 @@ class Router extends Component {
         const objetosPorPagina = 10;
 
         while (prevState.length >= 1) {
-            subSeparacion = prevState.splice(0, objetosPorPagina)
+            subSeparacion = prevState.splice(0, objetosPorPagina);
             separacion.push(subSeparacion);
         }
 
         if (separacion.length !== 0) {
-            return separacion
+            return separacion;
         } else {
-            return []
+            return [];
         }
+    };
 
-    }
-
-    listado = (separacion) => {
-
+    listado = separacion => {
         if (Boolean(separacion)) {
             if (this.state.loading) {
                 return (
                     <React.Fragment>
                         <h3 className="text-center">Cargando...</h3>
                     </React.Fragment>
-                )
+                );
             } else if (this.state.lista.length === 0) {
-                console.log("state.length = 0")
+                console.log("state.length = 0");
                 return (
                     <React.Fragment>
                         <BotonAdd />
                         <h3>No hay negocios para mostrar</h3>
-                    </React.Fragment>)
+                    </React.Fragment>
+                );
             } else {
-                return (
-                    this.paginas(separacion)
-                )
+                return this.paginas(separacion);
             }
         }
-    }
+    };
 
-    paginas = (lista) => {
+    paginas = lista => {
         //LISTA es el State separado (separacion).
 
         if (!Boolean(lista)) return null;
         const paginas = [...lista];
 
-
         return (
             <React.Fragment>
-                {
-                    paginas.map((pagina, i) => {
+                {paginas.map((pagina, i) => {
+                    if (i === 0) {
+                        i = "";
+                    } else {
+                        i += 1;
+                    }
 
-                        if (i === 0) {
-                            i = ""
-                        } else {
-                            i += 1;
-                        };
-
-                        return (
-                            <Route key={i} exact path={`/${i}`} render={() => {
+                    return (
+                        <Route
+                            key={i}
+                            exact
+                            path={`/${i}`}
+                            render={() => {
                                 return (
                                     <React.Fragment>
                                         <BotonAdd />
                                         <Listado lista={pagina} />
-                                        <Navegacion paginaActual={i} paginaFinal={paginas.length} />
+                                        <Navegacion
+                                            paginaActual={i}
+                                            paginaFinal={paginas.length}
+                                        />
                                     </React.Fragment>
-                                )
-                            }} />
-                        )
-                    })
-                }
+                                );
+                            }}
+                        />
+                    );
+                })}
             </React.Fragment>
-        )
-    }
+        );
+    };
 
     componentWillMount() {
-        this.fromDBtoState()
+        this.fromDBtoState();
     }
 
     // BOTONES -------------------
-    aceptar = (datos) => {
+    aceptar = datos => {
         // SE AÑADEN A LA BASE DE DATOS
         // ADDED TO DATABASE
 
         db.push(datos, this.fromDBtoState());
 
         this.borrar(datos);
-    }
+    };
 
-    borrar = (datos) => {
+    borrar = datos => {
         // REMOVED FROM DATABASE
-        dbMod.child(datos.key).remove()
-    }
+        dbMod.child(datos.key).remove();
+    };
 
-    add = (datos) => {
+    add = datos => {
         // ADDED TO DATABASE MODERATION LIST
-        const keyValue = dbMod.push().key
+        const keyValue = dbMod.push().key;
         datos["key"] = keyValue;
         dbMod.child(keyValue).update(datos);
-    }
+    };
     // ------------------ BOTONES
 
     render() {
-
         let separacion = [];
 
         if (Boolean(this.state.lista)) {
-            separacion = this.separacionPorPaginas(this.state.lista)
+            separacion = this.separacionPorPaginas(this.state.lista);
         }
 
-
-
         return (
-            <BrowserRouter basename={process.env.PUBLIC_URL} >
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div className="container">
                     <Header />
 
                     <Switch>
+                        <Route
+                            exact
+                            path={"/add"}
+                            render={() => {
+                                //AÑADIR POST
+                                return <Add add={this.add} />;
+                            }}
+                        />
 
-                        <Route exact path={"/add"} render={() => {
-                            //AÑADIR POST
-                            return (
-                                <Add add={this.add} />
-                            )
-                        }} />
-
-
-
-                        <Route exact path={"/listadomoderacion"} render={() => {
-                            //MODERACION
-                            return (
-                                <Moderacion objeto={this.state.listaPorModerar} aceptar={this.aceptar} borrar={this.borrar} />
-                            )
-                        }} />
+                        <Route
+                            exact
+                            path={"/listadomoderacion"}
+                            render={() => {
+                                //MODERACION
+                                return (
+                                    <Moderacion
+                                        objeto={this.state.listaPorModerar}
+                                        aceptar={this.aceptar}
+                                        borrar={this.borrar}
+                                    />
+                                );
+                            }}
+                        />
 
                         {this.listado(separacion)}
-
                     </Switch>
 
                     <Footer />
-
                 </div>
             </BrowserRouter>
         );
